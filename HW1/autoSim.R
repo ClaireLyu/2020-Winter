1f6b141d9ff6291d613de8e4173d7bfea7c853c0
Yuetong Lyu <y9lyu@g.ucla.edu>
2020-01-24 22:16:08 +0000

    Adjusted for the style.

--- HW1/autoSim.R
+++ HW1/autoSim.R
@@ -1,14 +1,17 @@
-# autoSim.R
-
+seed <- 203
+reps <- 50
 nVals <- seq(100, 500, by=100)
-dist = c("gaussian", "t1", "t5")
-for(d in dist){
+distTypes <- c("gaussian", "t1", "t5")
+
+for (d in 1:3) {
   for (n in nVals) {
-    oFile <- paste("n", n, ".txt", sep="")
-    arg = paste("n=", n, " dist=", shQuote(shQuote(dist)),
-                " seed=", seed, " reps=", reps, sep="")
-    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile, sep="")
+    oFile <- paste("n", n, "dist", distTypes[d], ".txt", sep = "")
+    arg = paste("n=", n, " dist=", shQuote(shQuote(distTypes[d])),
+                " seed=", seed, " reps=", reps, sep = "")
+    sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile, sep = "")
     system(sysCall, wait = FALSE)
-    print(paste("sysCall=", sysCall, sep=""))
+    print(paste("sysCall=", sysCall, sep = ""))
   }
 }
+
+
