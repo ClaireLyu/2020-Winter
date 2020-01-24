f47bfbb46b13b70cbcfbd5bbf5f615beb339c186
Yuetong Lyu <y9lyu@g.ucla.edu>
2020-01-24 00:39:05 +0000

    Introduced distribution types and added two additional arguments into the for loop.

---
+++ HW1/autoSim.R
@@ -0,0 +1,12 @@
+# autoSim.R
+
+nVals <- seq(100, 500, by=100)
+dist = c("gaussian", "t1", "t5")
+for (n in nVals) {
+  oFile <- paste("n", n, ".txt", sep="")
+  arg = paste("n=", n, " dist=", shQuote(shQuote(dist)),
+              " seed=", seed, " reps=", reps, sep="")
+  sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile, sep="")
+  system(sysCall, wait = FALSE)
+  print(paste("sysCall=", sysCall, sep=""))
+}
