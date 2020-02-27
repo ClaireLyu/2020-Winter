1b9427416ab8746552ad5d647386787feb4da829
y9lyu <y9lyu@35.236.33.238>
2020-02-27 00:21:43 +0000

    add help text and change title in each tab

--- HW3/ui.R
+++ HW3/ui.R
@@ -1,26 +1,52 @@
 #' @import shiny
 ui <- fluidPage(
-  
-  titlePanel("CoronaVirus Senser"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("case", "Case Category:",
-                   c("Confirmed" = "c",
-                     "Recovered" = "r",
-                     "Death" = "d")),
-      
-      dateInput("date", "Date:", value = "2020-02-25"), 
-      format = "mm/dd/yy"),
+  tabsetPanel(
+    tabPanel("Map", 
+             
+             titlePanel("Overview the Distribution of Corona Virus in China"),
+             
+             sidebarLayout(
+               sidebarPanel(
+                 helpText("Create demographic maps with 
+                          data from JHU on 2019-nCoV Outbreak."),
+                 
+                 radioButtons("case", "Select A Case Category:",
+                              c("Confirmed" = "c",
+                                "Recovered" = "r",
+                                "Death" = "d")
+                              ),
+                 
+                 dateInput("date", "Select a date:", value = "2020-02-25", 
+                           format = "mm/dd/yy"),
+               ),
+               
+               mainPanel(plotOutput("plot1"))
+               
+              )
+    ),
     
-    dateRangeInput("dates", 
-                   "Date range",
-                   start = "2020-01-22", 
-                   end = "2020-02-25")
-  ),
-  
-  mainPanel(
-    fluidRow(splitLayout(cellWidths = c("50%", "50%"), 
-                         plotOutput("plot1"), plotOutput("plot2")))
-  )
+    tabPanel("Line Graph", 
+             
+             titlePanel("Select Dates to Have a Closer Look..."),
+             
+             sidebarLayout(
+               sidebarPanel(
+                 helpText("Create a line graph with selected time interval."),
+                 
+                 dateRangeInput("dates", 
+                                "Select the range of date:",
+                                start = "2020-01-22", 
+                                end = "2020-02-25")
+               ),
+               mainPanel(plotOutput("plot2"))
+             )
+     ),
+    
+    tabPanel("Counts", 
+             
+             titlePanel("See How it Grew in Different Provinces"),
+             
+             flowLayout(plotOutput("plot3"))
+    )
+   )
 )
